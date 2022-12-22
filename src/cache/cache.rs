cb4125845622bf114c2db865c8afaf68c23513ab
u6940136 <u6940136@anu.edu.au>
2022-12-23 07:33:56 +1100

    Added cache policy trait

--- src/cache/cache.rs
+++ src/cache/cache.rs
@@ -1,4 +1,16 @@
+use std::fmt::Error;
+use crate::allocator::alloc_manager::{AllocManager};
 
+pub trait CachePolicy<T, K, V> {
+    fn new(alloc: &AllocManager, size: usize, options: Option<T>) -> Self;
+    fn destroy(&mut self, alloc: &AllocManager);
+    // TODO: Create custom error
+    fn request(&mut self, key: K, value: V) -> (Option<Error>, Option<V>);
+    fn get(&mut self, alloc: &AllocManager, key: K) -> (V, Option<V>);
+    fn contains(&self, key: K) -> bool;
+    fn is_full(&self) -> bool;
+
+}
 
 pub struct Cache {
 }
